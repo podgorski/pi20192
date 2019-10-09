2ccfb96efd24dba673fc81640f2a710bb23746a2
aluno <antonio@domob.me>
2019-10-09 12:23:10 -0300

    atualizei a frase

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,6 @@ const functions = require('firebase-functions');
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
 
- response.send("Hello from Firebase!");
+ response.send("Hello from Firebase 2!");
  
 });
