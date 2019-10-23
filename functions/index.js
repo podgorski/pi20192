4d947799b2d8ac79fd351b4cb30891b9e5c5d16d
Antonio Podgorski <podgorski1895@gmail.com>
2019-10-23 06:56:34 -0300

    Added solicitar hash cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
-
-
+const admin = require('firebase-admin');
+admin.initializeApp();
+const hash = require('object-hash');
+ 
 exports.helloWorld = functions.https.onRequest((request, response) => {
 
  response.send("Hello from Firebase 2!");
  
+});
+
+exports.criarHash = functions.https.onRequest((request, response) => {
+ 
+    const novoHash = hash({cpf: request.body.cpf});
+    admin.firestore().collection("aulas").doc(novoHash).set({
+        latitude: request.body.latitude,
+        longitude: request.body.longitude,
+        hash: novoHash,
+        cpf: request.body.cpf,
+        aberto: true,
+        criacao: admin.firestore.FieldValue.serverTimestamp()
+    }).then(writeResult => {
+        // write is complete here
+        response.json({
+            status : writeResult
+        });
+    });
+ 
+
 });
